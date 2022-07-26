221c6afb271eedcbe35be3a4826b24f526020d01
Millena Bomtempo <millena.bomtempo@gmail.com>
2022-07-26 15:55:12 -0300

    implement add task to list

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { ChangeEvent, FormEvent, useState } from 'react'
+import { v4 as uuidv4 } from 'uuid';
 import { Header } from './components/Header'
 import { AddTodoInput } from './components/AddTodoInput'
 import { TodoList } from './components/TodoList'
@@ -5,12 +7,43 @@ import { TodoList } from './components/TodoList'
 import styles from './App.module.css'
 import './global.css'
 
+export interface Task {
+  id: string
+  title: string
+  isCompleted: boolean
+}
+
 function App() {
+  const [addTask, setAddTask] = useState('')
+  const [tasks, setTasks] = useState<Task[]>([])
+  
+  function handleAddTask(e: ChangeEvent<HTMLInputElement>) {
+    e.preventDefault()
+    setAddTask(e.target.value)
+  }
+
+  function updateTasks(e: FormEvent){
+    e.preventDefault()
+    setTasks([
+      ...tasks, 
+      {
+        id: uuidv4(),
+        title: addTask,
+        isCompleted: false
+      }
+    ])
+    setAddTask('')
+  }
+
   return (
     <main className={styles.container}>
       <Header />
       <div className={styles.content}>
-        <AddTodoInput />
+        <AddTodoInput 
+          addTask={addTask}
+          onHandleAddTask={handleAddTask}
+          onHandleUpdateTasks={updateTasks}
+        />
 
         <TodoList />
       </div>
--- src/components/AddTodoInput.tsx
+++ src/components/AddTodoInput.tsx
@@ -1,15 +1,29 @@
 import { PlusCircle } from "phosphor-react";
+import { ChangeEvent, FormEvent } from "react";
 
 import styles from './AddTodoInput.module.css'
 
-export function AddTodoInput() {
+interface AddTodoInputProps {
+  addTask: string
+  onHandleAddTask: (e: ChangeEvent<HTMLInputElement>) => void
+  onHandleUpdateTasks: (e: FormEvent) => void
+}
+
+export function AddTodoInput({ addTask, onHandleAddTask, onHandleUpdateTasks}:AddTodoInputProps) {
   return (
-    <div className={styles.container}>
-      <input type="text" placeholder='Adicione uma nova tarefa'/>
-      <button type='submit'>
+    <form className={styles.container} onSubmit={onHandleUpdateTasks}>
+      <input 
+        type="text" 
+        placeholder='Adicione uma nova tarefa'
+        value={addTask}
+        onChange={onHandleAddTask}
+      />
+      <button 
+        type='submit'
+      >
         Criar
         <PlusCircle size={18}/>
       </button>
-    </div>
+    </form>
   )
 }
