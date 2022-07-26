c8c9216a4094a3db8a46f23a1c69150d0353b581
Millena Bomtempo <millena.bomtempo@gmail.com>
2022-07-26 14:57:44 -0300

    add created components to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import { Header } from './components/Header'
+import { ClipboardText, PlusCircle, Trash } from 'phosphor-react'
+
+import styles from './App.module.css'
 import './global.css'
+import { AddTodoInput } from './components/AddTodoInput'
+import TodoList from './components/TodoList'
 
 function App() {
   return (
-    <main>
+    <main className={styles.container}>
       <Header />
+      <div className={styles.content}>
+        <AddTodoInput />
+
+        <TodoList />
+      </div>
     </main>
   )
 }
